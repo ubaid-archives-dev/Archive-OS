f9f3ccfc299b5c2c475c533de18a4f0f2c2680b7
ubaid ur rehman <ubaid.archives@gmail.com>
2026-09-02 01:30:15 +0500

    changes in java script

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const osWindow = document.getElementById('window-1');
 const titleBar = document.getElementById('title-bar-1');
 const closeBtn = document.querySelector('.close-btn');
 const clock = document.getElementById('clock');
+const startBtn = document.getElementById('start-btn'); 
 
 setInterval(() => {
     const now = new Date();
@@ -12,21 +13,32 @@ closeBtn.addEventListener('click', () => {
     osWindow.style.display = 'none';
 });
 
+
+startBtn.addEventListener('click', () => {
+    osWindow.style.display = 'flex';
+});
+
 let isDragging = false;
 let offsetX = 0;
 let offsetY = 0;
 
 function startDrag(e) {
     isDragging = true;
-
-    osWindow.style.transform = 'none';
-
+    
+    const rect = osWindow.getBoundingClientRect();
+    
+    osWindow.style.left = `${rect.left}px`;
+    osWindow.style.top = `${rect.top}px`;
+    
+    osWindow.style.transform = 'none'; 
+    
     const clientX = e.type.includes('touch') ? e.touches[0].clientX : e.clientX;
     const clientY = e.type.includes('touch') ? e.touches[0].clientY : e.clientY;
     
-    offsetX = clientX - osWindow.getBoundingClientRect().left;
-    offsetY = clientY - osWindow.getBoundingClientRect().top;
+    offsetX = clientX - rect.left;
+    offsetY = clientY - rect.top;
 }
+
 function drag(e) {
     if (!isDragging) return;
 
@@ -35,7 +47,6 @@ function drag(e) {
     const clientX = e.type.includes('touch') ? e.touches[0].clientX : e.clientX;
     const clientY = e.type.includes('touch') ? e.touches[0].clientY : e.clientY;
     
-
     osWindow.style.left = `${clientX - offsetX}px`;
     osWindow.style.top = `${clientY - offsetY}px`;
 }
@@ -52,4 +63,3 @@ document.addEventListener('touchmove', drag, { passive: false });
 
 document.addEventListener('mouseup', stopDrag);
 document.addEventListener('touchend', stopDrag);
-
